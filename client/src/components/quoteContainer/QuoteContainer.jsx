9234512715360380af87b5b35aec2b8ed21f4f4a
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2024-03-04 11:23:58 +0000

    components: twitterButton added

--- client/src/components/quoteContainer/QuoteContainer.jsx
+++ client/src/components/quoteContainer/QuoteContainer.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import QuoteText from '../quoteText/QuoteText';
 import QuoteAuthor from '../quoteAuthor/QuoteAuthor';
+import TwitterButton from '../twitterButton/TwitterButton';
 
 const QuoteContainer = () => {
   return (
     <section className="quoteContainer__container">
       <QuoteText/>
       <QuoteAuthor/>
+      <TwitterButton/>
     </section>
   )
 }
---
+++ client/src/components/twitterButton/TwitterButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { RiTwitterXLine } from "react-icons/ri";
+
+const TwitterButton = () => {
+  return (
+    <button className="twitterButton__button" title="Tweet This!">
+        <RiTwitterXLine/>
+    </button>
+  )
+}
+
+export default TwitterButton
